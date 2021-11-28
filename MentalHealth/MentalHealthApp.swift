42f5831c0c64f2c76c394126dd5cedcd4e4d1865
Isac Tomaz da Silva <its@cin.ufpe.br>
2021-11-28 15:48:47 -0300

    Initial Commit

---
+++ MentalHealth/MentalHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalHealthApp.swift
+//  MentalHealth
+//
+//  Created by Isac Tomaz da Silva on 28/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MentalHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
