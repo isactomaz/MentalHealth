00779afe7b5ceef9c2554f4ee4d56f996679d862
Isac Tomaz da Silva <its@cin.ufpe.br>
2021-11-28 17:05:01 -0300

    Test View

---
+++ TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  MentalHealth
+//
+//  Created by Isac Tomaz da Silva on 28/11/21.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
