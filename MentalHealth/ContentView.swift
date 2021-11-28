5960b1df300af4fd82cc1a6efd76bec326a72f7f
Isac Tomaz da Silva <its@cin.ufpe.br>
2021-11-28 16:18:55 -0300

    Light and dark mode preview

--- MentalHealth/ContentView.swift
+++ MentalHealth/ContentView.swift
@@ -17,5 +17,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
